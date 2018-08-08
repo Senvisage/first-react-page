62b3ee7ab6527b461ee6a5eda43edee4341f2de0
Senvisage <senvisage.spam@gmail.com>
2018-08-08 23:58:22 +0200

    Minor CSS fixes

--- src/components/CharacterListItem.js
+++ src/components/CharacterListItem.js
@@ -5,7 +5,10 @@ import './CharacterListItem.css'
 const CharacterListItem = ({ name, level, onClick, isDead=false }) => (
   <li className="CharacterListItem" onClick={() => onClick(name)}>
     <div className="container">
-      <img src={`img/${name}_icon.png`} alt={`${name}`} className={isDead?"dead":""}/>
+      <img src={`img/${name}_icon.png`} alt={`${name}`} className={isDead?"dead icon":"icon"} />
+      {isDead && (
+        <img className="youAreDead" src="img/deceased.png" alt="YOU ARE DEAD." />
+      )}
     </div>
     <p>
       {name}
