ec37d19dc8fa0e406fa70d2c7bcea85ab98b46d7
Senvisage <senvisage.spam@gmail.com>
2018-08-09 01:27:22 +0200

    Add a functionnal Kill button in the Details Had to refactor a lot of code for this one...

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,29 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react';
 
 import './CharacterDetails.css'
 
-const CharacterDetails = ({ character }) => (
-  <div className="CharacterDetails">
-    <figure>
-      <img src={`img/${character.name}.png`} alt={`${character.name}`} />
-      <figcaption>
-        {character.name}
-      </figcaption>
-      {character.isDead && (
-        <img className="youAreDead" src="img/deceased.png" alt="YOU ARE DEAD."/>
-      )}
-    </figure>
+class CharacterDetails extends Component {
 
-    <div className="characterInfo">
-      <ul>
-        <li><strong>Name:</strong>&nbsp;&nbsp;&nbsp;{character.name}</li>
-        <li><strong>Level:</strong>&nbsp;&nbsp;{character.level}</li>
-        <li><strong>Status:</strong>&nbsp;{character.isDead ? "Dead" : "Wanted"}</li>
-      </ul>
-      <div className="story">
-        {character.story || "..."}
+  handleKillButtonClick = () => {
+    this.props.killSwitch(this.props.character.name);
+  }
+
+  render() {
+    const {character} = this.props;
+    return (
+      <div className="CharacterDetails">
+        <figure>
+          <img src={`img/${character.name}.png`} alt={`${character.name}`} />
+          <figcaption>
+            {character.name}
+          </figcaption>
+          {character.isDead && (
+            <img className="youAreDead" src="img/deceased.png" alt="YOU ARE DEAD."/>
+          )}
+        </figure>
+
+        <div className="characterInfo">
+          <ul>
+            <li><strong>Name:</strong>&nbsp;&nbsp;&nbsp;{character.name}</li>
+            <li><strong>Level:</strong>&nbsp;&nbsp;{character.level}</li>
+            <li><strong>Status:</strong>&nbsp;{character.isDead ? "Dead" : "Wanted"}
+              {!character.isDead && (
+                <button onClick={this.handleKillButtonClick}>I KILLED IT !</button>
+              )}
+            </li>
+          </ul>
+          <div className="story">
+            {character.story || "..."}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    );
+  }
+}
 export default CharacterDetails
