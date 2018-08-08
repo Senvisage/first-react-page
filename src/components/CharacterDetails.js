cd7412414c2ec380d4ed406e76cfb18b8baeaf8d
Senvisage <senvisage.spam@gmail.com>
2018-08-08 19:21:15 +0200

    #wip Adds a CharacterDetails component, and changes it's content according to the state

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -3,8 +3,24 @@ import React from 'react'
 import './CharacterDetails.css'
 
 const CharacterDetails = ({ character }) => (
-  <p className="CharacterDetails">
-    {character.name}
-  </p>
+  <div className="CharacterDetails">
+    <figure>
+      <img src={`img/${character.name}.png`} alt={`${character.name}`} className={character.isDead?"dead":""} />
+      <figcaption>
+        {character.name}
+      </figcaption>
+    </figure>
+    <div className="characterInfo">
+      <ul>
+        <li><strong>Name:</strong>&nbsp;&nbsp;&nbsp;{character.name}</li>
+        <li><strong>Level:</strong>&nbsp;&nbsp;{character.level}</li>
+        <li><strong>Status:</strong>&nbsp;{character.isDead ? "Dead" : "Wanted"}</li>
+      </ul>
+      <div className="story">
+        {character.story || "..."}
+      </div>
+    </div>
+  </div>
+
 )
 export default CharacterDetails
