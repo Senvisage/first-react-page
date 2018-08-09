ae76e2c1b2c8268252e9065d48041a23e3f9f2c3
Senvisage <senvisage.spam@gmail.com>
2018-08-09 14:09:22 +0200

    Correcting a mistake of default behaviour description

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -10,38 +10,33 @@ class CharacterDetails extends Component {
 
   render() {
     const {character} = this.props;
-    if(character !== undefined) {
-      return (
-        <div className="CharacterDetails">
-          <figure>
-            <img src={`img/${character.name}.png`} alt={`${character.name}`} />
-            <figcaption>
-              {character.name}
-            </figcaption>
-            {character.isDead && (
-              <img className="youAreDead" src="img/deceased.png" alt="YOU ARE DEAD."/>
-            )}
-          </figure>
+    return (
+      <div className="CharacterDetails">
+        <figure>
+          <img src={`img/${character.name}.png`} alt={`${character.name}`} />
+          <figcaption>
+            {character.name}
+          </figcaption>
+          {character.isDead && (
+            <img className="youAreDead" src="img/deceased.png" alt="YOU ARE DEAD."/>
+          )}
+        </figure>
 
-          <div className="characterInfo">
-            <ul>
-              <li><strong>Name:</strong>&nbsp;&nbsp;&nbsp;{character.name}</li>
-              <li><strong>Level:</strong>&nbsp;&nbsp;{character.level}</li>
-              <li><strong>Status:</strong>&nbsp;{character.isDead ? "Dead" : "Wanted"}
-                {!character.isDead && (
-                  <button className="killSwitch" onClick={this.handleKillButtonClick}>I KILLED IT !</button>
-                )}
-              </li>
-            </ul>
-            <div className="story">
-              {character.story || "..."}
-            </div>
+        <div className="characterInfo">
+          <ul>
+            <li><strong>Name:</strong>&nbsp;&nbsp;&nbsp;{character.name}</li>
+            <li><strong>Level:</strong>&nbsp;&nbsp;{character.level}</li>
+            <li><strong>Status:</strong>&nbsp;{character.isDead ? "Dead" : "Wanted"}
+              {!character.isDead && (
+                <button className="killSwitch" onClick={this.handleKillButtonClick}>I KILLED IT !</button>
+              )}
+            </li>
+          </ul>
+          <div className="story">
+            {character.story || "..."}
           </div>
         </div>
-      );
-    }
-    return (
-      <p>Choisis un Héros !</p>
+      </div>
     );
   }
 }
